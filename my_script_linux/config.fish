e58012f24feca7d51c2069e76862d47d801180a8
Fabio Scannavino <fabio@itgoa-cl-001.hexagonmetrology.com>
2021-09-18 22:30:56 +0200

    per fish

---
+++ my_script_linux/config.fish
@@ -0,0 +1,13 @@
+
+export MSC_LICENSE_FILE="/msc/MSC.Software/MSC.Licensing/Lithium/license.dat"
+set PATH /home/fabio/.local/bin /home/fabio/bin /home/fabio/.local/usr/local/bin $PATH
+
+alias ls=exa
+alias l=exa
+alias ll="exa -l"
+
+set -g theme_nerd_fonts yes
+set -g fish_prompt_pwd_dir_length 0
+set -g theme_newline_cursor yes
+set -g theme_newline_prompt '$ '
+set -g theme_color_scheme solarized
