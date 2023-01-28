9146ce3a261b754a514364bcd3c10b529d3621f9
FabioScannavino <72655214+fabioHexagon@users.noreply.github.com>
2023-01-28 17:33:22 +0100

    vai

--- SCOOP/crea_lista_scoop.bat
+++ SCOOP/crea_lista_scoop.bat
@@ -1,2 +1,3 @@
-scoop list | grep "global" > scoop_software.txt
-cp lista_software.txt c:\Users\e3018\vim_main\scoop_software.txt 
+scoop list > scoop_software.txt
+rem scoop list | grep "global" > scoop_software.txt
+rem cp lista_software.txt c:\Users\e3018\vim_main\scoop_software.txt 
