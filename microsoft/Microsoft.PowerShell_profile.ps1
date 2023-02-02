2b75f562176307c14fdccb521131eed3099b13dc
FabioScannavino <72655214+fabioHexagon@users.noreply.github.com>
2023-02-02 23:15:49 +0100

    adding

--- microsoft/Microsoft.PowerShell_profile.ps1
+++ microsoft/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,18 @@
-Set-PSReadLineOption -PredictionSource History
-oh-my-posh --init --shell pwsh --config "~/.mytheme.omp.json" | Invoke-Expression
-oh-my-posh --init --shell pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
 
-#Set-Alias -Name ls -Value "wsl ls --color=auto -X"
-function ls_alias { wsl ls --color=auto -hF $args }
-Set-Alias -Name ls -Value ls_alias -Option AllScope
+# function ls_alias { wsl ls --color=auto -hF $args }
+# Set-Alias -Name ls -Value ls_alias -Option AllScope
 
-Set-Alias -Name np -Value C:\Windows\notepad.exe
-Set-Alias -Name note++ -Value "C:\Program Files\Notepad++\notepad++.exe" 
+function ll_alias { ls --color=auto -hFl $args }
+Set-Alias -Name ll -Value ll_alias -Option AllScope
 
-# run_marc_2021.3="C:\MSC.Software\Marc\2021.3.1\marc2021.3.1\tools\run_marc" $*
-# mentat_2021.3=start "" "C:\MSC.Software\Marc\2021.3.1\mentat2021.3.1\bin\mentat.bat"
-# fortran_config2017="C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2019.4.245\windows\bin\ipsxe-comp-vars.bat" intel64 vs2017
-# fortran_local="D:\PROGRAMMI\cmder\vendor\fortran_local.bat"
-# fortran_server="D:\PROGRAMMI\cmder\vendor\fortran_server.bat"
+# Set-PoshPrompt -Theme aliens
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
---
+++ microsoft/backup.bat
@@ -0,0 +1,2 @@
+cp C:\Users\e3018\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 .
+cp "C:\Program Files\Nilesoft\Shell\shell.shl" .
