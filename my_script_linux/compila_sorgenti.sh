c386f0a17874bbb8d0b28beaa22f9f444acf0917
FabioScannavino <72655214+fabioHexagon@users.noreply.github.com>
2023-02-02 23:16:21 +0100

    ee

---
+++ my_script_linux/compila_sorgenti.sh
@@ -0,0 +1,5 @@
+export PATH=$PATH:~/.local/usr/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/usr/lib
+export C_INCLUDE_PATH=$C_INCLUDE_PATH:~/.local/usr/include
+export CPLUS_INCLUDE_PATH=$CPLUS_INCLUDE_PATH:~/.local/usr/include
+export LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/usr/lib
